5e904877b73ac93d966dca0c8087b174ad0f8edf
Sumeet Singh <ortuna@gmail.com>
2012-09-05 22:56:26 -0400

    Init commit

---
+++ extend.rb
@@ -0,0 +1,35 @@
+require "fnordmetric"
+
+FnordMetric.namespace :myapp do
+  hide_active_users
+
+
+  timeseries_gauge :number_of_signups,
+    :title => "Number of Signups",
+    :series => [:via_twitter, :via_facebook, :other],
+    :resolution => 1.minute
+
+  widget 'TechStats', {
+    :title => "Events Numbers",
+    :type => :numbers,
+    :width => 100,
+    :gauges => [:number_of_signups],
+    :offsets => [1,3,5,10],
+    :autoupdate => 1
+  }
+
+  event :go do
+    puts data
+    if data[:ref] == 'twitter'
+      incr :number_of_signups, :via_twitter,1
+    elsif data[:ref] == 'facebook'
+      incr :number_of_signups, :via_facebook,1
+    else
+      incr :number_of_signups, :other,1
+    end
+  end
+
+  # echo '{"_type": "go","ref":"facebzz"}' | nc localhost 1337
+end
+
+FnordMetric.standalone
